786820d7dfb3b83a606bd67130a07d2f8110c3a5
howhowlin1996 <et6068591234e@gmail.com>
2020-04-29 17:32:51 +0800

    new function the puzzles will bounce back to the start position every 500ms

--- app/src/main/java/com/example/writing/puzzle/Puzzle.java
+++ app/src/main/java/com/example/writing/puzzle/Puzzle.java
@@ -5,15 +5,22 @@ import android.os.Build;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
-
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.example.writing.R;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
 
 public class Puzzle extends AppCompatActivity implements View.OnTouchListener {
     private float begin_x,begin_y;
     private int move_x,move_y;
+    private long begin_time=0,final_time=0;
+    private int[]begin_l=new int [11];
+    private int[]begin_t=new int[11];
+    private Map<Integer,Integer>idMap=new HashMap<>();
+    private Queue<Integer>puzzlequeue=new LinkedList<Integer>();
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,9 +36,10 @@ public class Puzzle extends AppCompatActivity implements View.OnTouchListener {
         PuzzlePanel down3=findViewById(R.id.charDown3);
         PuzzlePanel down4=findViewById(R.id.charDown4);
         AnswerBoard answerBoard=findViewById(R.id.answerBoard);
+        PuzzlePanelGroup group=findViewById(R.id.Group);
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {         //set panel background for copying the character
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {            //set panel background for copying the character
             qu1.setBackground(getDrawable(R.drawable.no));
             qu2.setBackground(getDrawable(R.drawable.longlong));
             up1.setBackground(getDrawable(R.drawable.longlong_copy));
@@ -54,16 +62,35 @@ public class Puzzle extends AppCompatActivity implements View.OnTouchListener {
         down3.setOnTouchListener(this);
         down4.setOnTouchListener(this);
 
+        for (int i=0;i<11;i++){
+            idMap.put(group.getChildAt(i).getId(),i);
+        }
+
+
+
     }
+
 
 
     @Override
     public boolean onTouch(View v, MotionEvent event) {
+        int idnum=v.getId();
 
-
+        if (final_time==0){
+            saveInfoPos();
+        }
+        if (puzzlequeue.size()!=0) {
+                if (!puzzlequeue.contains(idnum)){
+                    puzzlequeue.add(idnum);
+                }
+        }
+        else{
+            puzzlequeue.add(idnum);
+        }
         switch (event.getAction()){
 
             case MotionEvent.ACTION_DOWN:
+
                 begin_x=event.getX();
                 begin_y=event.getY();
 
@@ -71,16 +98,45 @@ public class Puzzle extends AppCompatActivity implements View.OnTouchListener {
                 move_x=(int)(event.getRawX()-begin_x);                                              /*2*begin_y??*/
                 move_y=(int)(event.getRawY()-2*begin_y);
                 v.layout(move_x,move_y,move_x+v.getWidth(),move_y+v.getHeight());
-            case MotionEvent.ACTION_UP:
-
-                break;
         }
 
+        final_time=System.currentTimeMillis();
+
+        if (final_time-begin_time>500&&begin_time!=0&&puzzlequeue.size()!=0){                       // the system will set the puzzles to the start position every 500ms
+            int size=puzzlequeue.size();
+            for (int i=0;i<size;i++){
+                if (puzzlequeue.element()!=idnum){
+                    backToStart(puzzlequeue.element());
+                    puzzlequeue.remove();
+                }
+            }
+        }
+
+        if (begin_time==0){
+            begin_time=System.currentTimeMillis();
+        }
 
         return true;
     }
 
 
+
+    public void saveInfoPos(){                                                                      //The work of this function is to remember the begin position of every views by using array
+        PuzzlePanelGroup group=findViewById(R.id.Group);
+        for (int i=0;i<11;i++){
+            begin_l[i]=group.begin_l[i];
+            begin_t[i]=group.begin_t[i];
+        }
+    }
+
+
+
+    public void backToStart(int id){                                                                 //The work of this function is to set the puzzles to the begin position
+        PuzzlePanelGroup group=findViewById(R.id.Group);
+        int idnum=idMap.get(id);
+        View child=group.getChildAt(idnum);
+        child.layout(begin_l[idnum],begin_t[idnum],begin_l[idnum]+child.getWidth(),begin_t[idnum]+child.getHeight());
+    }
 
 
 }
--- app/src/main/java/com/example/writing/puzzle/PuzzlePanelGroup.java
+++ app/src/main/java/com/example/writing/puzzle/PuzzlePanelGroup.java
@@ -1,19 +1,18 @@
 package com.example.writing.puzzle;
 
 import android.content.Context;
-import android.provider.Settings;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
-import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 
-public class PuzzlePanelGroup extends ViewGroup {
+public class PuzzlePanelGroup extends ViewGroup {                                                   /*PuzzleGroup is for puzzle package only, it is used to align the puzzle view. These group contains 11puzzle views*/
     private Context this_context;
-    int width,height,viewgroup_h;
-    public PuzzlePanelGroup(Context context, AttributeSet attrs) {
+    int width,height,viewgroup_h;                                                                   //width means the width of screen, height means the same.
+    public final  int[]begin_l=new int [11];                                                        //these two array use for storing the begin left and top position of 11 views
+    public  final int[]begin_t=new int[11];
+    public PuzzlePanelGroup(Context context, AttributeSet attrs) {                                  //to measure the screen size
         super(context, attrs);
         this_context=context;
         DisplayMetrics dm = new DisplayMetrics();
@@ -25,46 +24,53 @@ public class PuzzlePanelGroup extends ViewGroup {
 
 
     @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {                         //to set the group to meet the screen
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         measureChildren(widthMeasureSpec,heightMeasureSpec);
         setMeasuredDimension(width,height);
     }
 
     @Override
-    protected void onLayout(boolean changed, int l, int t, int r, int b) {
-        int left_pos=0;
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {                          //to set the position of 11 layouts
+        int left_pos=0;                                                                             //the panel index is arranged to be 0 deliberately because it needs to be last layout
         View sizeview=getChildAt(1);
         int puzzleHeight=sizeview.getMeasuredHeight();
         int puzzleWidth=sizeview.getMeasuredWidth();
-        for (int i=1;i<3;i++){
-            View childview=getChildAt(i);
+        for (int i=1;i<3;i++){                                                                      //to set the question puzzle
+            View childview=getChildAt(i);                                                           //there are two question puzzles which are on the top of screen
             left_pos+=puzzleWidth;
             viewgroup_h=0;
             childview.layout(left_pos,viewgroup_h,left_pos+puzzleWidth,viewgroup_h+puzzleHeight);
+            begin_l[i]=left_pos;
+            begin_t[i]=viewgroup_h;
         }
         left_pos=0;
-        for (int i=3;i<7;i++){
+        for (int i=3;i<7;i++){                                                                      // to set the radical puzzles which are in the upper part of screen
             View childview=getChildAt(i);
             viewgroup_h=puzzleHeight;
             childview.layout(left_pos,viewgroup_h,left_pos+puzzleWidth,viewgroup_h+puzzleHeight);
+            this.begin_l[i]=left_pos;
+            this.begin_t[i]=viewgroup_h;
             left_pos+=puzzleWidth;
         }
         left_pos=0;
-        for (int i=0;i<1;i++){
+        for (int i=0;i<1;i++){                                                                      //to set the puzzlepanel in the middle of screen
             View Paneled=getChildAt(i);
             int PanelHeight= Paneled.getMeasuredHeight();
             int PanelWidth=Paneled.getMeasuredWidth();
-            Log.d("error",new String(PanelHeight+"///////"+PanelWidth));
             left_pos=puzzleWidth;
             viewgroup_h*=3;
             Paneled.layout(left_pos,viewgroup_h,left_pos+PanelWidth,viewgroup_h+PanelHeight);
+            begin_l[i]=left_pos;
+            begin_t[i]=viewgroup_h;
         }
         left_pos=0;
         for (int i=7;i<11;i++){
             View childview=getChildAt(i);
             viewgroup_h=height-2*puzzleHeight;
-            childview.layout(left_pos,viewgroup_h,left_pos+puzzleWidth,viewgroup_h+puzzleHeight);
+            childview.layout(left_pos,viewgroup_h,left_pos+puzzleWidth,viewgroup_h+puzzleHeight);            //to set the radical puzzles which are in the bottom of screen
+            begin_l[i]=left_pos;
+            begin_t[i]=viewgroup_h;
             left_pos+=puzzleWidth;
         }
 
@@ -72,6 +78,10 @@ public class PuzzlePanelGroup extends ViewGroup {
 
 
     }
+
+
+
+
 
 
 }
